5531316e9b9c2bc46e620ef572c08a27842b1589
Matheus Henrique <matheus.hsr95@gmail.com>
2020-11-09 15:23:43 -0300

    update

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const methodOverride = require('method-override')
 
 const server = express();
 
 
 server.use(express.urlencoded({extended:true})) //Responsável pelo req.body
 server.use(express.static('public'));
+server.use(methodOverride('_method'))
 server.use(routes);
 
 server.set("view engine", "njk");
--- teachers.js
+++ teachers.js
@@ -65,6 +65,7 @@ exports.show = function(req,res){
     
 }
 
+//edit
 exports.edit = function( req,res ){
 
     const {id} = req.params
@@ -80,4 +81,48 @@ exports.edit = function( req,res ){
     }
     
     return res.render("teachers/edit", { teacher })
+}
+
+//put
+exports.put = function(req,res){
+    const {id} = req.body
+    let index = 0
+
+    const foundTeachers = data.teachers.find(function (teacher, foundIndex){
+        if (id == teacher.id)
+        index = foundIndex
+        return true
+    })
+    if(!foundTeachers) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeachers,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("write error!")
+
+        return res.redirect(`/teachers/${teacher.id}`)
+    })
+}
+
+//delete
+exports.delete = function(req,res){
+    const { id } = req.body
+
+    const filteredTeacher = data.teachers.filter(function (teacher) {
+        return  teacher.id != id 
+    })
+
+    data.teachers = filteredTeacher
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("write error!")
+
+        return res.redirect("/teachers")
+    })
 }
